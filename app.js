1cd21e96082849a1833257e6a63d1e25dfbec6e7
Eduardo Melendez <cz34101@hotmail.com>
2024-11-27 16:06:32 -0600

    V1

--- app.js
+++ app.js
@@ -16,7 +16,182 @@ const MongoAdapter = require("@bot-whatsapp/database/mongo");
 const MONGO_DB_URI = "mongodb://localhost/ti-tools-back";
 const MONGO_DB_NAME = "db_bot";
 
+//----------------------------------------------------------------FLUJO COMPLETO REQUISICIONES----------------------------------------------------------------
+//FLUJO HIJO Ceco o Insumo
+const flowCecos = addKeyword(["4", "Cuatro"])
+  .addAnswer(
+    "Si no se encuentra el ceco o insumo que necessita, favor de comunicarse con la Gerencia de Procura"
+  )
+  .addAnswer("Para regresar al menu, escribe Menu");
+
+//FLUJO HIJO Usuario o Contraseña Incorrecta
+const flowPassSao = addKeyword(["3", "Tres"])
+  .addAnswer([
+    "*Comunicate con el equipo de TI para checar su cuenta*",
+    "Juan Miguel Aguirre Paredes -- *Coordinador de TI* -- 📞993 190 8780",
+    "Cesar Eduardo Zepeda Melendez -- *Becario de TI* -- 📞993 189 4332",
+  ])
+  .addAnswer("Para regresar al menu, escribe Menu");
+
+//FLUJO HIJO ¿Cual es el Link de la plataforma de Next?
+const flowLinkSao = addKeyword(["2", "Dos"])
+  .addAnswer(" *PORTAL DE REQUSICIONES* https://sao.grupolamat.com.mx", {
+    media:
+      "https://www.manageengine.com/products/service-desk/help-desk-software/images/what-is-a-support-ticket-system.png",
+  })
+  .addAnswer("Para regresar al menu, escribe Menu");
+
+//FLUJO HIJO No tengo cuenta de Sao
+const flowNoCuentaSao = addKeyword(["1", "Uno"])
+  .addAnswer([
+    "*Comunicate con el equipo de TI para la creacion de su cuenta*",
+    "Juan Miguel Aguirre Paredes -- *Coordinador de TI* -- 📞993 190 8780",
+    "Cesar Eduardo Zepeda Melendez -- *Becario de TI* -- 📞993 189 4332",
+  ])
+  .addAnswer("Para regresar al menu, escribe Menu");
+
+// FLUJO PRINCIPAL DE MENU DE CORREO
+const flowSao = addKeyword(["6", "Seis"]).addAnswer(
+  [
+    "¿Que problema tienes?",
+    "*1*.-No tengo cuenta del portal de Requisiciones",
+    "*2*.-¿Cual es el Link de la plataforma de Requisiciones?",
+    "*3*.-Usuario o Contraseña Incorrecta",
+    "*4*.-Ceco o insumo faltante",
+    "Para regresar al menu principal, escribe *Menu*",
+  ],
+  null,
+  null,
+  [flowNoCuentaSao, flowLinkSao, flowPassSao, flowCecos]
+);
+
+//----------------------------------------------------------------FLUJO COMPLETO DE NEXTCLOUD----------------------------------------------------------------
+//FLUJO HIJO Usuario o Contraseña Incorrecta
+const flowPassNext = addKeyword(["3", "Tres"])
+  .addAnswer([
+    "*Comunicate con el equipo de TI para checar su cuenta*",
+    "Rafael de la Cruz Ramos -- *Auxiliar de TI* -- 📞 993 190 8780",
+    "Izamer Sanchez Perez -- *Auxiliar de TI* -- 📞 993 243 7599",
+    "Edgar Eduardo Jimenez -- *Becario de TI* -- 📞993 277 8223",
+    "Cesar Eduardo Zepeda Melendez -- *Becario de TI* -- 📞993 189 4332",
+  ])
+  .addAnswer("Para regresar al menu, escribe Menu");
+
+//FLUJO HIJO ¿Cual es el Link de la plataforma de Next?
+const flowLinkNext = addKeyword(["2", "Dos"])
+  .addAnswer([
+    "*Links de Nextcloud:*",
+    "*Area de Procura, Admon Obra y Contabilidad* https://nextcloud0.grupolamat.com.mx/",
+    "*Area de Control Integral y Recursos Humanos* https://nextcloud1.grupolamat.com.mx/",
+    "*Area Construccion y Tecnica* https://nextcloud2.grupolamat.com.mx/",
+    "*Area Admon y Finanzas, Juridico y Maquinaria* https://nextcloud3.grupolamat.com.mx/",
+  ])
+  .addAnswer("Para regresar al menu, escribe Menu");
+
+//FLUJO HIJO No tengo cuenta de Next
+const flowNoCuentaNext = addKeyword(["1", "Uno"])
+  .addAnswer([
+    "*Comunicate con el equipo de TI para la creacion de su cuenta*",
+    "Rafael de la Cruz Ramos -- *Auxiliar de TI* -- 📞 993 190 8780",
+    "Izamer Sanchez Perez -- *Auxiliar de TI* -- 📞 993 243 7599",
+    "Edgar Eduardo Jimenez -- *Becario de TI* -- 📞993 277 8223",
+    "Cesar Eduardo Zepeda Melendez -- *Becario de TI* -- 📞993 189 4332",
+  ])
+  .addAnswer("Para regresar al menu, escribe Menu");
+
+// FLUJO PRINCIPAL DE MENU DE CORREO
+const flowNext = addKeyword(["5", "Cinco"]).addAnswer(
+  [
+    "¿Que problema tienes?",
+    "*1*.-No tengo cuenta de Nextcloud",
+    "*2*.-¿Cual es el Link de la plataforma de Nextcloud?",
+    "*3*.-Usuario o Contraseña Incorrecta",
+    "Para regresar al menu principal, escribe *Menu*",
+  ],
+  null,
+  null,
+  [flowNoCuentaNext, flowLinkNext, flowPassNext]
+);
+
+//----------------------------------------------------------------FLUJO COMPLETO TICKETS----------------------------------------------------------------
+
+//FLUJO HIJO Usuario o Contraseña Incorrecta
+const flowPassTickets = addKeyword(["3", "Tres"])
+  .addAnswer([
+    "*Comunicate con el equipo de TI para checar su cuenta*",
+    "Rafael de la Cruz Ramos -- *Auxiliar de TI* -- 📞 993 190 8780",
+    "Izamer Sanchez Perez -- *Auxiliar de TI* -- 📞 993 243 7599",
+    "Edgar Eduardo Jimenez -- *Becario de TI* -- 📞993 277 8223",
+    "Cesar Eduardo Zepeda Melendez -- *Becario de TI* -- 📞993 189 4332",
+  ])
+  .addAnswer("Para regresar al menu, escribe Menu");
+
+//FLUJO HIJO ¿Cual es el Link de la plataforma de Tickets?
+const flowLinkTickets = addKeyword(["2", "Dos"])
+  .addAnswer(" *TICKETS* https://helpdesk.grupolamat.com", {
+    media:
+      "https://nextcloud3.grupolamat.com.mx/core/preview?fileId=2437405&x=1920&y=1080&a=true&etag=ef7a3bb872dec28854729f3b4c3dc788",
+  })
+  .addAnswer("Para regresar al menu, escribe Menu");
+
+//FLUJO HIJO No tengo cuenta de Tickets
+const flowNoCuentaTickets = addKeyword(["1", "Uno"])
+  .addAnswer([
+    "*Comunicate con el equipo de TI para la creacion de su cuenta*",
+    "Rafael de la Cruz Ramos -- *Auxiliar de TI* -- 📞 993 190 8780",
+    "Izamer Sanchez Perez -- *Auxiliar de TI* -- 📞 993 243 7599",
+    "Edgar Eduardo Jimenez -- *Becario de TI* -- 📞993 277 8223",
+    "Cesar Eduardo Zepeda Melendez -- *Becario de TI* -- 📞993 189 4332",
+  ])
+  .addAnswer("Para regresar al menu, escribe Menu");
+
+// FLUJO PRINCIPAL DE MENU DE CORREO
+const flowTickets = addKeyword(["4", "Cuatro"]).addAnswer(
+  [
+    "¿Que problema tienes?",
+    "*1*.-No tengo cuenta de Tickets",
+    "*2*.-¿Cual es el Link de la plataforma de Tickets?",
+    "*3*.-Usuario o Contraseña Incorrecta",
+    "Para regresar al menu principal, escribe *Menu*",
+  ],
+  null,
+  null,
+  [flowNoCuentaTickets, flowLinkTickets, flowPassTickets]
+);
+
 // ----------------------------------------------------------------FLUJO COMPLETO CORREO----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// FLUJO HIJO Error Your Box is Full
+const flowBoxLimitError = addKeyword(["7", "Siete"])
+  .addAnswer(
+    "El error *Your Box is Full* es esta relacionado con la capacidad de almacenamiento de una cuenta de correo electrónico. Este mensaje indica que la bandeja de entrada del usuario (la *caja* o *mailbox*) ha alcanzado su límite máximo de almacenamiento y no puede recibir nuevos correos"
+  )
+  .addAnswer([
+    "*Comunicate con el equipo de TI para realizar el respaldo de su correo:*",
+    "Rafael de la Cruz Ramos -- *Auxiliar de TI* -- 📞 993 190 8780",
+    "Izamer Sanchez Perez -- *Auxiliar de TI* -- 📞 993 243 7599",
+    "Edgar Eduardo Jimenez -- *Becario de TI* -- 📞993 277 8223",
+    "Cesar Eduardo Zepeda Melendez -- *Becario de TI* -- 📞993 189 4332",
+  ])
+  .addAnswer("Para regresar al menu, escribe Menu");
+
+// FLUJO HIJO Error 550 Recipient correo has exceeded mailbox limit
+const flowReceiptError = addKeyword(["6", "Seis"])
+  .addAnswer(
+    "El Error 550 Recipient has exceeded mailbox limit es un mensaje de error de correo electrónico que indica que el destinatario no puede recibir el correo porque ha superado el límite de almacenamiento asignado a su bandeja de entrada. Este límite puede estar definido por el proveedor de correo electrónico o por la configuración del servidor de correo de la organización del destinatario."
+  )
+  .addAnswer([
+    "*Soluciones:*",
+    "*1*.-Comunicarse con TI para notificar que correo ya no tiene espacio",
+    "*2*.-Al redactar el correo eliminar al destinatario que tiene el error para poder enviar el correo",
+  ])
+  .addAnswer([
+    "*Comunicate con el equipo de TI:*",
+    "Rafael de la Cruz Ramos -- *Auxiliar de TI* -- 📞 993 190 8780",
+    "Izamer Sanchez Perez -- *Auxiliar de TI* -- 📞 993 243 7599",
+    "Edgar Eduardo Jimenez -- *Becario de TI* -- 📞993 277 8223",
+    "Cesar Eduardo Zepeda Melendez -- *Becario de TI* -- 📞993 189 4332",
+  ])
+  .addAnswer("Para regresar al menu, escribe Menu");
 
 // FLUJO HIJO Error 550 Message Too Big
 const flowErrorBig = addKeyword(["5", "cinco"])
@@ -40,60 +215,34 @@ const flowFirma = addKeyword(["4", "cuarto", "firma"])
 
 // FLUJO HIJO Usuario o Contraseña Incorrecta
 const flowUserPassCorreo = addKeyword(["3", "tres", "incorrecto"])
-  .addAnswer("Comunicate con el equipo de TI:")
-  .addAnswer(
-    "Juan Miguel Aguirre Paredes -- *Auxiliar de TI* -- 📞993 190 8780",
-    {
-      media: "https://intn24.lalr.co/old/Up.jpg",
-    }
-  )
-  .addAnswer("Izamer Sanchez Perez -- *Auxiliar de TI* -- 📞993 243 7599", {
-    media:
-      "https://static.wikia.nocookie.net/disney/images/e/e2/Don_Risitas.png/revision/latest?cb=20130423093527&path-prefix=es",
-  })
-  .addAnswer("Edgar Eduardo Jimenez -- *Becario de TI* -- 📞993 277 8223", {
-    media:
-      "https://www.rollingstone.com/wp-content/uploads/2020/07/Screen-Shot-2020-07-15-at-11.24.37-AM.jpg?w=910&h=511&crop=1",
-  })
-  .addAnswer(
+  .addAnswer([
+    "*Comunicate con el equipo de TI:*",
+    "Rafael de la Cruz Ramos -- *Auxiliar de TI* -- 📞 993 190 8780",
+    "Izamer Sanchez Perez -- *Auxiliar de TI* -- 📞 993 243 7599",
+    "Edgar Eduardo Jimenez -- *Becario de TI* -- 📞993 277 8223",
     "Cesar Eduardo Zepeda Melendez -- *Becario de TI* -- 📞993 189 4332",
-    {
-      media:
-        "https://img.wattpad.com/85fdf423872032e520fa1319c98c40aab069f8a6/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f41616962797a5f553372466272413d3d2d3836342e313761633162366163643335356130313135313134313232353839342e6a7067?s=fit&w=720&h=720",
-    }
-  )
+  ])
   .addAnswer("Para regresar al menu, escribe Menu");
 
 // FLUJO HIJO ¿Cual es el Link de la plataforma de correo?
 const flowLinkCorreo = addKeyword(["2", "link", "plataforma"])
-  .addAnswer(" *CORREO* https://grupolamat.com.mx", {
-    media:
-      "https://images.vexels.com/media/users/3/136340/isolated/preview/74ac661d8216442ae469efb39c9584dc-icono-de-mensaje-de-correo.png",
-  })
+  .addAnswer(" *CORREO* https://grupolamat.com.mx")
   .addAnswer("Para regresar al menu, escribe Menu");
 
 // FLUJO HIJO No me deja abrir el Outlook'
-const flowOutlook = addKeyword("1", "outlook", "uno")
+const flowOutlook = addKeyword("1", "outlook", "uno", "atras")
   .addAnswer(
     "*Primer Paso* Abrir el administrador de tareas con un clic derecho en la barra de tareas"
   )
   .addAnswer(
-    "*Segundo paso* En la pestaña de procesos buscar la tarea con nombre Microsoft Outlook",
-    {
-      media:
-        "https://www.ionos.mx/digitalguide/fileadmin/DigitalGuide/Screenshots_2023/windows-task-manager-end-outlook.png",
-    }
+    "*Segundo paso* En la pestaña de procesos buscar la tarea con nombre Microsoft Outlook"
   )
   .addAnswer(
     "*Tercer paso* Da clic derecho en ese proceso y damos clic en *Finalizar la tarea* "
   )
   .addAnswer("*Cuarto paso* Vuelva a abrir la aplicacion Outlook")
   .addAnswer(
-    "Si el error persiste, favor de comunicarte con TI en https://helpdesk.grupolamat.com o en el menu principal en el apartado de *Contactos de TI*",
-    {
-      media:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCQjDyyRr5xcRxl0TCoFG85hmI2jyIJreTng&s",
-    }
+    "Si el error persiste, favor de comunicarte con TI en https://helpdesk.grupolamat.com o en el menu principal en el apartado de *Contactos de TI*"
   )
   .addAnswer("Para regresar al menu principal, escribe *Menu*");
 
@@ -112,51 +261,34 @@ const flowCorreo = addKeyword(["3", "tres", "regresar"]).addAnswer(
   ],
   null,
   null,
-  [flowOutlook, flowLinkCorreo, flowUserPassCorreo, flowFirma, flowErrorBig]
+  [
+    flowOutlook,
+    flowLinkCorreo,
+    flowUserPassCorreo,
+    flowFirma,
+    flowErrorBig,
+    flowReceiptError,
+    flowBoxLimitError,
+  ]
 );
 
 // ----------------------------------------------------------------FLUJO ENLANCES----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 const flowlinks = addKeyword(["2", "dos"])
-  .addAnswer(" *TICKETS* https://helpdesk.grupolamat.com", {
-    media:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCQjDyyRr5xcRxl0TCoFG85hmI2jyIJreTng&s",
-  })
-  .addAnswer(" *CORREO* https://grupolamat.com.mx", {
-    media:
-      "https://images.vexels.com/media/users/3/136340/isolated/preview/74ac661d8216442ae469efb39c9584dc-icono-de-mensaje-de-correo.png",
-  })
-  .addAnswer(" *PORTAL DE REQUSICIONES* https://sao.grupolamat.com.mx", {
-    media:
-      "https://www.manageengine.com/products/service-desk/help-desk-software/images/what-is-a-support-ticket-system.png",
-  })
+  .addAnswer(" *TICKETS* https://helpdesk.grupolamat.com")
+  .addAnswer(" *CORREO* https://grupolamat.com.mx")
+  .addAnswer(" *PORTAL DE REQUSICIONES* https://sao.grupolamat.com.mx")
   .addAnswer("Para regresar al menu, escribe *Menu*");
 
 // ----------------------------------------------------------------FLUJO CONTACTOS----------------------------------------------------------------
 const flowcontactos = addKeyword(["1", "uno", "contactos"])
-  .addAnswer("Juan Alberto Calderon -- *Gerente de GAF*", {
-    media:
-      "https://i.ytimg.com/vi/wYJW07pgLN0/oar2.jpg?sqp=-oaymwEYCJUDENAFSFqQAgHyq4qpAwcIARUAAIhC&rs=AOn4CLAiudfkwGb2gHz08EG94AKqVndIXw",
-  })
   .addAnswer(
-    "Juan Miguel Aguirre Paredes -- *Coordinador de TI* -- 📞993 190 8780",
-    {
-      media: "https://intn24.lalr.co/old/Up.jpg",
-    }
+    "Juan Miguel Aguirre Paredes -- *Coordinador de TI* -- 📞993 190 8780"
   )
-  .addAnswer("Izamer Sanchez Perez -- *Auxiliar de TI* -- 📞993 243 7599", {
-    media:
-      "https://static.wikia.nocookie.net/disney/images/e/e2/Don_Risitas.png/revision/latest?cb=20130423093527&path-prefix=es",
-  })
-  .addAnswer("Edgar Eduardo Jimenez -- *Becario de TI* -- 📞993 277 8223", {
-    media:
-      "https://www.rollingstone.com/wp-content/uploads/2020/07/Screen-Shot-2020-07-15-at-11.24.37-AM.jpg?w=910&h=511&crop=1",
-  })
+  .addAnswer("Rafael de la Cruz Ramos -- *Auxiliar de TI* -- 📞993 104 4174")
+  .addAnswer("Izamer Sanchez Perez -- *Auxiliar de TI* -- 📞993 243 7599")
+  .addAnswer("Edgar Eduardo Jimenez -- *Becario de TI* -- 📞993 277 8223")
   .addAnswer(
-    "Cesar Eduardo Zepeda Melendez -- *Becario de TI* -- 📞993 189 4332",
-    {
-      media:
-        "https://img.wattpad.com/85fdf423872032e520fa1319c98c40aab069f8a6/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f41616962797a5f553372466272413d3d2d3836342e313761633162366163643335356130313135313134313232353839342e6a7067?s=fit&w=720&h=720",
-    }
+    "Cesar Eduardo Zepeda Melendez -- *Becario de TI* -- 📞993 189 4332"
   )
   .addAnswer("Para regresar al menu, escribe *Menu*");
 
@@ -175,7 +307,7 @@ const flowPrincipal = addKeyword(["hola", "buenos", "dias", "tardes", "menu"])
     ],
     null,
     null,
-    [flowcontactos, flowlinks, flowCorreo]
+    [flowcontactos, flowlinks, flowCorreo, flowTickets, flowNext, flowSao]
   );
 
 const main = async () => {
